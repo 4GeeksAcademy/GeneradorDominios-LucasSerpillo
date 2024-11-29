2f5b55ae2ecf255400acf3cbb2b467bb4e7003ab
lucas serpillo <88857502+lucasSerpillo@users.noreply.github.com>
2024-11-29 11:28:36 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["The", "Our", "Its", "His"];
+let adj = ["Great", "Big", "Smart", "Bad"];
+let noun = ["Jogger", "Racoon", "Money", "Street"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[p] + "-" + adj[a] + "-" + noun[n] + ".com");
+      }
+    }
+  }
 };
